982b91cb056e97044344b534e589632f0ee86f67
Wagner <wagneraraujo15@gmail.com>
2022-06-10 11:03:43 -0300

    test .env datas

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -25,11 +25,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "oregon-postgres.render.com"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi_h6xe"),
-      user: env("DATABASE_USERNAME", "strapi_h6xe_user"),
-      password: env("DATABASE_PASSWORD", "kZhHvb1DCqJMM2TskhUNdJ1JSLU99qVY"),
+      host: env("DATABASE_HOST", process.env.DATABASE_HOST),
+      port: env.int("DATABASE_PORT", process.env.DATABASE_PORT),
+      database: env("DATABASE_NAME", process.env.DATABASE_NAME),
+      user: env("DATABASE_USERNAME", process.env.DATABASE_USERNAME),
+      password: env("DATABASE_PASSWORD", process.env.DATABASE_PASSWORD),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
         ssl: env("DATABASE_SSL", true),
