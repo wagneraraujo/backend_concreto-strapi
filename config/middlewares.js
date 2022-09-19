a703878fb7aac27213253a7acd12318e6da24977
Wagner Araujo <wagneraraujo15@gmail.com>
2022-09-18 21:05:55 -0300

    update collaborattor inputs

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,4 +8,18 @@ module.exports = [
   'strapi::body',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
